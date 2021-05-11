ca94cd13064d1b99719cf0a880a215b429858750
Leouarz <ghalielouarzazi@hotmail.com>
2021-05-11 18:32:31 +0200

    first commit

---
+++ src/components/contact.jsx
@@ -0,0 +1,167 @@
+import React from "react";
+import imageOverlay from "../img/earth.jpg";
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <section
+        className="paralax-mf footer-paralax bg-image sect-mt4 route"
+        style={{ backgroundImage: "url(" + imageOverlay + ")" }}
+      >
+        <div className="overlay-mf"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="contact-mf">
+                <div id="contact" className="box-shadow-full">
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="title-box-2">
+                        <h5 className="title-left">Send A Message</h5>
+                      </div>
+                      <div>
+                        <form
+                          action="https://formspree.io/f/mvodkgpe"
+                          method="POST"
+                          className="contactForm"
+                        >
+                          <div id="sendmessage">
+                            Your message has been sent. Thank you!
+                          </div>
+                          <div id="errormessage"></div>
+                          <div className="row">
+                            <div className="col-md-12 mb-3">
+                              <div className="form-group">
+                                <input
+                                  type="text"
+                                  name="name"
+                                  className="form-control"
+                                  id="name"
+                                  placeholder="Your Name"
+                                  required
+                                  maxlength="120"
+                                />
+                                <div className="validation"></div>
+                              </div>
+                            </div>
+                            <div className="col-md-12 mb-3">
+                              <div className="form-group">
+                                <input
+                                  type="email"
+                                  className="form-control"
+                                  name="email"
+                                  id="email"
+                                  placeholder="Your Email"
+                                  required
+                                  maxlength="500"
+                                />
+                                <div className="validation"></div>
+                              </div>
+                            </div>
+                            <div className="col-md-12 mb-3">
+                              <div className="form-group">
+                                <input
+                                  type="text"
+                                  className="form-control"
+                                  name="subject"
+                                  id="subject"
+                                  placeholder="Subject"
+                                  required
+                                  minlength="4"
+                                  maxlength="200"
+                                />
+                                <div className="validation"></div>
+                              </div>
+                            </div>
+                            <div className="col-md-12 mb-3">
+                              <div className="form-group">
+                                <textarea
+                                  className="form-control"
+                                  name="message"
+                                  rows="5"
+                                  required
+                                  maxlength="500"
+                                  placeholder="Message"
+                                ></textarea>
+                                <div className="validation"></div>
+                              </div>
+                            </div>
+                            <div className="col-md-12">
+                              <button
+                                type="submit"
+                                className="button button-a button-big button-rouded"
+                              >
+                                Send Message
+                              </button>
+                            </div>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="title-box-2 pt-4 pt-md-0">
+                        <h5 className="title-left">Get in Touch</h5>
+                      </div>
+                      <div className="more-info">
+                        <p className="lead">
+                          Whether you want to get in touch, talk about a project
+                          collaboration, or just say hi, I'd love to hear from
+                          you.
+                          <br />
+                          Simply fill the from and send me an email.
+                        </p>
+                        <ul class="list-ico">
+                          <li><a target="_blank" rel="noreferrer" href="https://www.google.com/maps/place/80+Boulevard+Flandrin,+75116+Paris/data=!4m2!3m1!1s0x47e66559adbf837b:0xdd02578b5d0a8d5?sa=X&ved=2ahUKEwjr9bCo0cHwAhWRMBQKHZbfCUsQ8gEwAHoECAYQAQ"><span style={{paddingRight: "4px"}} class="ion-ios-location"></span>80 Boulevard Flandrin, 75116, Paris</a></li>
+                          <li><a href="tel:+33634171934"><span style={{paddingRight: "2px"}} class="ion-ios-telephone"></span>(+33) 6 34 17 19 34</a></li>
+                          <li><a target="_blank" rel="noreferrer" href="mailto:g.elouarzaz@aliansactfrance.com"><span class="ion-email"></span>g.elouarzaz@aliansactfrance.com</a></li>
+                        </ul>
+                      </div>
+                      <div className="socials">
+                        <ul>
+                          <li>
+                            <a
+                              href="https://github.com/Leouarz"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <span className="ico-circle">
+                                <i className="ion-social-github"></i>
+                              </span>
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="https://www.linkedin.com/in/ghali-el-ouarzazi/"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <span className="ico-circle">
+                                <i className="ion-social-linkedin"></i>
+                              </span>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                <div className="copyright-box">
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </section>
+    );
+  }
+}
+
+export default Contact;
